6c4d1830aac34ef8ca1994fda6d88e27d61ead50
James <jamzdarren@gmail.com>
2022-03-13 22:20:28 -0500

    Updated files per exercise instructions

--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,10 @@
 // Paste in the provided code here:
+export class SpaceLocation {
+    name: string;
+    kilometersAway: number;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+ }
